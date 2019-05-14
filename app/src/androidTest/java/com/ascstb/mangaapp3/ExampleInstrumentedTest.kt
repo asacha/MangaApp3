5e2ae6e373c05a24f300281200e6ece2485edd03
“arturo.sanchez2” <arturo.sanchez@volvocars.com>
2019-05-13 20:39:05 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/ascstb/mangaapp3/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.ascstb.mangaapp3
+
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.ascstb.mangaapp3", appContext.packageName)
+    }
+}
